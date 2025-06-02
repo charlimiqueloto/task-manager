4d4cca77ce8b41e19518d1b2fda05c0952a1abbc
jmcha <sj.char@gmail.com>
2025-06-02 11:30:42 -0300

    created script to initialize server

---
+++ startup.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Starting Docker container...
+docker start taskmanager-mongo
+
+echo Waiting 5 seconds for Mongo to be ready
+timeout /t 5
+
+echo Running seed.js
+node seed.js
+
+echo Starting the server with nodemon
+npm run dev
